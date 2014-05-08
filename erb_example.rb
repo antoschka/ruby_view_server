79e4925a54a83dbfd927e3e2b3873d2cf18c5bb9
Anton <antoschka@gmail.com>
2014-05-08 20:09:52 +0200

    homework section 1 completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,7 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+name = {:Anton => "Arnst", :Toni => "Zimmer"}
+adresse = ["eins", 2]
+templateName = ERB.new "Der nachname von Anton ist <%= name[:Anton]%> und von Toni <%= name[:Toni]%	>"
+puts templateName.result(binding)
